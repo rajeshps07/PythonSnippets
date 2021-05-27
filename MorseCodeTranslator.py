889f1ea42242e8b06ff03b8f8331cd6f50a45838
rajeshps07 <72856346+rajeshps07@users.noreply.github.com>
2021-05-27 06:57:11 +0530

    Update MorseCodeTranslator.py

--- MorseCodeTranslator.py
+++ MorseCodeTranslator.py
@@ -1,16 +1,16 @@
 # Morse Code Translator
 
 
-# VARIABLE KEY
-# 'cipher' -> 'stores the morse translated form of the english string'
-# 'decipher' -> 'stores the english translated form of the morse string'
-# 'citext' -> 'stores morse code of a single character'
-# 'i' -> 'keeps count of the spaces between morse characters'
-# 'message' -> 'stores the string to be encoded or decoded'
+# variables description
+# morse: stores the morse translated form of the english string
+# english: stores the english translated form of the morse string
+# engtext: stores morse code of a single character
+# i: keeps count of the spaces between morse characters
+# message: stores the string to be encoded or decoded
 
 
 # Dictionary representing the morse code chart
-MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+morse_code_dict = {'A': '.-', 'B': '-...',
                    'C': '-.-.', 'D': '-..', 'E': '.',
                    'F': '..-.', 'G': '--.', 'H': '....',
                    'I': '..', 'J': '.---', 'K': '-.-',
@@ -27,35 +27,33 @@ MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    '(': '-.--.', ')': '-.--.-'}
 
 
-# Function to encrypt the string
-# according to the morse code chart
 def encrypt(message):
-    cipher = ''
+    """
+    function to encrypt the string according to the morse code chart
+    """
+    morse = ''
     for letter in message:
         if letter != ' ':
-
-            # Looks up the dictionary and adds the
-            # corresponding morse code
-            # along with a space to separate
-            # morse codes for different characters
-            cipher += MORSE_CODE_DICT[letter] + ' '
+            # looks up the dictionary and adds the corresponding morse code
+            # along with a space to separate morse codes for different characters
+            morse += morse_code_dict[letter] + ' '
         else:
-            # 1 space indicates different characters
-            # and 2 indicates different words
-            cipher += ' '
+            # 1 space indicates different characters & 2 indicates different words
+            morse += ' '
 
-    return cipher
+    return morse
 
 
-# Function to decrypt the string
-# from morse to english
 def decrypt(message):
-    # extra space added at the end to access the
-    # last morse code
-    message += ' '
+    """
+    function to decrypt the string from morse to english
+    """
+    # extra space added at the end to access the last morse code
 
-    decipher = ''
-    citext = ''
+    message += ' '
+    english = ''
+    engtext = ''
+
     for letter in message:
 
         # checks for space
@@ -65,7 +63,7 @@ def decrypt(message):
             i = 0
 
             # storing morse code of a single character
-            citext += letter
+            engtext += letter
 
         # in case of space
         else:
@@ -76,27 +74,31 @@ def decrypt(message):
             if i == 2:
 
                 # adding space to separate words
-                decipher += ' '
+                english += ' '
             else:
 
                 # accessing the keys using their values (reverse of encryption)
-                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
-                                                              .values()).index(citext)]
-                citext = ''
+                english += list(morse_code_dict.keys())[list(morse_code_dict
+                                                             .values()).index(engtext)]
+                engtext = ''
 
-    return decipher
+    return english
 
 
 # Main function
 def main():
-    # print("Do you want to encode or decode a message?")
-    sel = input("Enter your selection - 'encode' or 'decode' below:\n")
+    print("Do you want to translate your message 'to Morse Code' or 'to English'?")
+    sel = input("Enter your selection - \n"
+                "a: 'to Morse Code'\n"
+                "b: 'to English'\n")
     message = input("Enter your message below:\n")
-    if sel == "encode":
+    if sel == "a":
         result = encrypt(message.upper())
+        print("Your translated message in Morse Code:")
         print(result)
     else:
         result = decrypt(message)
+        print("Your translated message in English:")
         print(result)
 
 
